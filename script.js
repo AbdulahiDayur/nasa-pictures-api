452e01790fce725cdbef2eeb7e6f5c05d499b80a
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-03-26 13:46:51 -0700

    remove functionality for favorites

--- script.js
+++ script.js
@@ -41,8 +41,13 @@ function createDOMNodes(page) {
     // SAVE
     const addToFav = document.createElement('p');
     addToFav.classList.add('clickable');
-    addToFav.textContent = 'Add to Favorites';
-    addToFav.setAttribute('onclick', `saveFavorite('${result.url}')`);
+    if (page === 'favorites') {
+      addToFav.textContent = 'Remove From Favorites';
+      addToFav.setAttribute('onclick', `removeFavorite('${result.url}')`);
+    } else {
+      addToFav.textContent = 'Add to Favorites';
+      addToFav.setAttribute('onclick', `saveFavorite('${result.url}')`);
+    }
     // <p class="card-text">
     const cardText = document.createElement('p');
     cardText.classList.add('card-text');
@@ -71,9 +76,9 @@ function updateDOM(page) {
   // get Favs from local storage
   if (localStorage.getItem('nasaFavorites')) {
     favorites = JSON.parse(localStorage.getItem('nasaFavorites'));
-    console.log('LocalStorage Favorites: \n', favorites);
-    // console.log(favorites);
+    // console.log('LocalStorage Favorites: \n', favorites);
   }
+  imagesContainer.textContent = '';
   createDOMNodes(page);
 }
 
@@ -83,7 +88,7 @@ async function getNasaPictures() {
     const res = await axios.get(apiUrl);
     const data = await res.data;
     resultsArray = data;
-    updateDOM('favorites');
+    updateDOM('results');
   } catch (error) {
     console.log('Whoops', error);
   }
@@ -106,5 +111,17 @@ function saveFavorite(itemUrl) {
   });
   // console.log(favorites);
 }
+
+// Remove item from Favorites
+function removeFavorite(itemUrl) {
+  if (favorites[itemUrl]) {
+    delete favorites[itemUrl];
+    // Set Favorites in LocalStorage
+    localStorage.setItem('nasaFavorites', JSON.stringify(favorites));
+    updateDOM('favorites');
+  }
+  console.log('FavArray After Deleting Item\n', favorites);
+}
+
 
 getNasaPictures();
